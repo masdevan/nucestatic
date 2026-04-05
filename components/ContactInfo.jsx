294804c1efbe2fb5d54d3ac04753dedb1ef37202
Mas Devan <masdevansugiharta@gmail.com>
2026-04-05 23:04:49 +0700

    update pattern

--- components/ContactInfo.jsx
+++ components/ContactInfo.jsx
@@ -1,8 +1,18 @@
 export default function ContactInfo() {
   return (
-    <div className="bg-[#76090e]">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
+    <div
+      className="relative"
+      style={{
+        backgroundImage: "url('/pattern.jpg')",
+        backgroundRepeat: "repeat",
+        backgroundSize: "300px",
+      }}
+    >
+      <div className="absolute inset-0 bg-black/50" />
+
+      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
         <div className="flex flex-col sm:flex-row sm:justify-center sm:items-start space-y-16 sm:space-y-0 sm:space-x-10 py-12">
+          
           <div className="text-xl text-[#b6b6b6] text-center sm:text-left">
             <strong className="text-white">Ampel</strong><br />
             Nucestatic<br />
@@ -10,6 +20,7 @@ export default function ContactInfo() {
             Ampel, Boyolali, Jawa Tengah 57352<br />
             Indonesia<br />
           </div>
+
           <div className="text-xl text-[#b6b6b6] text-center sm:text-left">
             <strong className="text-white">Boyolali</strong><br />
             Jl. Boyolali - Solo<br />
@@ -17,6 +28,7 @@ export default function ContactInfo() {
             Indonesia<br />
             +62 857 2813 3473
           </div>
+
         </div>
       </div>
     </div>
