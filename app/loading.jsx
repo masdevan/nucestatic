1e78b10bab34a17ed59bb2ce2cb2686b7a432b82
Mas Devan <masdevansugiharta@gmail.com>
2026-08-20 22:03:44 +0700

    fix: remove unused files

--- app/loading.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
-      <div className="w-8 h-8 border-2 border-[#800000] border-t-transparent rounded-full animate-spin" />
-    </div>
-  );
-}
